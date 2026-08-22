1048bf9d020c0d2e6e529fb36bf76eae52847005
Randy Girard <rgirard59@yahoo.com>
2026-08-22 14:57:11 -0400

    chore: regenerate wails bindings under go/app

--- frontend/wailsjs/go/models.ts
+++ frontend/wailsjs/go/models.ts
@@ -1,27 +1,4 @@
-export namespace logbuf {
-	
-	export class Entry {
-	    time: string;
-	    level: string;
-	    message: string;
-	    attrs?: string;
-	
-	    static createFrom(source: any = {}) {
-	        return new Entry(source);
-	    }
-	
-	    constructor(source: any = {}) {
-	        if ('string' === typeof source) source = JSON.parse(source);
-	        this.time = source["time"];
-	        this.level = source["level"];
-	        this.message = source["message"];
-	        this.attrs = source["attrs"];
-	    }
-	}
-
-}
-
-export namespace main {
+export namespace app {
 	
 	export class EqHostState {
 	    current: string;
@@ -247,6 +224,146 @@ export namespace main {
 	        this.latest = source["latest"];
 	        this.release_url = source["release_url"];
 	        this.error = source["error"];
+	    }
+	}
+
+}
+
+export namespace keys {
+	
+	export class Accelerator {
+	    Key: string;
+	    Modifiers: string[];
+	
+	    static createFrom(source: any = {}) {
+	        return new Accelerator(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.Key = source["Key"];
+	        this.Modifiers = source["Modifiers"];
+	    }
+	}
+
+}
+
+export namespace logbuf {
+	
+	export class Entry {
+	    time: string;
+	    level: string;
+	    message: string;
+	    attrs?: string;
+	
+	    static createFrom(source: any = {}) {
+	        return new Entry(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.time = source["time"];
+	        this.level = source["level"];
+	        this.message = source["message"];
+	        this.attrs = source["attrs"];
+	    }
+	}
+
+}
+
+export namespace menu {
+	
+	export class MenuItem {
+	    Label: string;
+	    Role: number;
+	    Accelerator?: keys.Accelerator;
+	    Type: string;
+	    Disabled: boolean;
+	    Hidden: boolean;
+	    Checked: boolean;
+	    SubMenu?: Menu;
+	
+	    static createFrom(source: any = {}) {
+	        return new MenuItem(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.Label = source["Label"];
+	        this.Role = source["Role"];
+	        this.Accelerator = this.convertValues(source["Accelerator"], keys.Accelerator);
+	        this.Type = source["Type"];
+	        this.Disabled = source["Disabled"];
+	        this.Hidden = source["Hidden"];
+	        this.Checked = source["Checked"];
+	        this.SubMenu = this.convertValues(source["SubMenu"], Menu);
+	    }
+	
+		convertValues(a: any, classs: any, asMap: boolean = false): any {
+		    if (!a) {
+		        return a;
+		    }
+		    if (a.slice && a.map) {
+		        return (a as any[]).map(elem => this.convertValues(elem, classs));
+		    } else if ("object" === typeof a) {
+		        if (asMap) {
+		            for (const key of Object.keys(a)) {
+		                a[key] = new classs(a[key]);
+		            }
+		            return a;
+		        }
+		        return new classs(a);
+		    }
+		    return a;
+		}
+	}
+	export class Menu {
+	    Items: MenuItem[];
+	
+	    static createFrom(source: any = {}) {
+	        return new Menu(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.Items = this.convertValues(source["Items"], MenuItem);
+	    }
+	
+		convertValues(a: any, classs: any, asMap: boolean = false): any {
+		    if (!a) {
+		        return a;
+		    }
+		    if (a.slice && a.map) {
+		        return (a as any[]).map(elem => this.convertValues(elem, classs));
+		    } else if ("object" === typeof a) {
+		        if (asMap) {
+		            for (const key of Object.keys(a)) {
+		                a[key] = new classs(a[key]);
+		            }
+		            return a;
+		        }
+		        return new classs(a);
+		    }
+		    return a;
+		}
+	}
+
+}
+
+export namespace options {
+	
+	export class SecondInstanceData {
+	    Args: string[];
+	    WorkingDirectory: string;
+	
+	    static createFrom(source: any = {}) {
+	        return new SecondInstanceData(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.Args = source["Args"];
+	        this.WorkingDirectory = source["WorkingDirectory"];
 	    }
 	}
 
